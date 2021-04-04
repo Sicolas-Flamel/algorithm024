aa7f73de86171e3e744b8244689fe130df9d430d
Sicolas-Flamel <76936556+Sicolas-Flamel@users.noreply.github.com>
2021-04-04 17:26:39 +0800

    Update 146 LRU缓存机制.py

--- Week_09/146 LRU缓存机制.py
+++ Week_09/146 LRU缓存机制.py
@@ -23,3 +23,86 @@ class LRUCache(object):
         
         # 替换算法
         
+# 用双向链表实现的版本：
+class DLinkedNode:
+    def __init__(self, key=0, value=0):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+
+class LRUCache:
+    
+    def __init__(self, capacity: int):
+        self.cache = dict()
+        # 使用伪头部和伪尾部节点
+        self.head = DLinkedNode()
+        self.tail = DLinkedNode()
+        self.head.next = self.tail 
+        self.tail.prev = self.head
+        self.capacity = capacity
+        self.size = 0
+        
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        # 存在就提到最前面
+        node = self.cache[key]
+        self.moveToHead(node)
+        return node.value
+    
+    def put(self, key: int, value: int) -> None:
+        if key not in self.cache:
+            # if not exists, then create a new node
+            node = DLinkedNode(key, value)
+            # add to the Hash 
+            self.cache[key] = node
+            # add to the doublelinklist's head
+            self.addToHead(node)
+            self.size += 1
+            if self.size > self.capacity:
+                # if > volumn, delete the last node
+                removed = self.removeTail()
+                # del the exact item
+                self.cache.pop(removed.key)
+                self.size -= 1
+            else:
+                # key exist, hash->get, then move to the head
+                node = self.cache[key]
+                node.value = value
+                self.moveToHead(node)
+                
+        def addToHead(self, node):
+            node.prev = self.head   # 这里就是把它移到最前面来了。
+            node.next = self.head.next
+            self.head.next.prev = node
+            self.head.next = node
+            
+        def removeNode(self, node):
+            node.prev.next = node.next
+            node.next.prev = node.prev
+            
+        def moveToHead(self, node):
+            self.removeNode(node)
+            self.addToHead(node)
+            
+        def removeTail(self):
+            node = self.tail.prev
+            self.removeNode(node)
+            return node
+            
+            
+            
+            
+            
+            
+            
+            
+        
+        
+        
+        
+        
+        
+        
+        
